d30f3a51cf65e820ab2e488b6d3c441f2f631370
rwiley <30681834+Cerkoryn@users.noreply.github.com>
2025-05-05 15:55:50 -0400

    Added explainer for Current model

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,18 @@
-<h1>This is using the current forumula for saturation cap on Cardano.</h1>
-<p>Experiment with the sliders to visualize the effects of changes to parameters.</p>
+<div class="grid grid-flow-col auto-cols-max gap-8">
+  <div class="flex flex-col gap-0.5 leading-tight">
+    <h1 class="m-0">This is using the current formula for saturation cap on Cardano.</h1>
+    <p class="m-0">Experiment with the sliders to visualize the effects of changes to parameters.</p>
+  </div>
 
-<h2>Legend</h2>
-<p>C0 - Saturation Cap</p>
-<p>S - Total Supply</p>
-<p>R - Reserve</p>
-<p>z0 - Relative pool saturation size (1/k)</p>
+  <div class="flex flex-col gap-0.5 leading-tight">
+    <h2 class="m-0 font-bold underline">Legend</h2>
+    <p class="m-0">C<sub>0</sub> - Saturation Cap</p>
+    <p class="m-0">S - Total Supply</p>
+    <p class="m-0">R - Reserve</p>
+    <p class="m-0">z<sub>0</sub> - Relative pool saturation size (1/k)</p>
+  </div>
+
+  <div class="flex flex-col gap-1 justify-center items-center">
+    <img src="/Current.png" alt="Formula: C0 = (S - R) z0" />
+  </div>
+</div>
